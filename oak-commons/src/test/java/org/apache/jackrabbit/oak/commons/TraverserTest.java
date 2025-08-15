6bd93ba2a8cb997a88db97ab94c5a12c5d4a90cc
Julian Reschke <julian.reschke@gmx.de>
2025-08-15 09:26:14 +0100

    OAK-11832 : replaced Guava's TreeTraversal with OAK Commons - fix Import in testcase

--- oak-commons/src/test/java/org/apache/jackrabbit/oak/commons/TraverserTest.java
+++ oak-commons/src/test/java/org/apache/jackrabbit/oak/commons/TraverserTest.java
@@ -19,7 +19,7 @@
 
 package org.apache.jackrabbit.oak.commons;
 
-import com.google.common.collect.TreeTraverser;
+import org.apache.jackrabbit.guava.common.collect.TreeTraverser;
 import org.apache.commons.collections4.FluentIterable;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
